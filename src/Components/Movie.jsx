1618d96d7b2862469695b220e0a1b1199f611fca
JuniorBaymax <cooladitya953@gmail.com>
2020-09-29 21:19:14 +0530

    Movie data fatched

--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import '../index.css';
 
 const IMG_API = 'https://image.tmdb.org/t/p/w1280';
 
-const Movie = ({title, poster_path, overview, vote_average}) => {
+const Movie = ({ title, poster_path, overview, vote_average }) => {
   return (
-    
-  <div className='movie'>
-    <img src={IMG_API + poster_path} alt={title} />
-  </div>
-  )
+    <div className="movie">
+      <img src={IMG_API + poster_path} alt={title} />
+      <div className="movie-info">
+        <h3>{title}</h3>
+        <span>{vote_average}</span>
+      </div>
+
+      <div className="movie-overview">
+        <h2>Overview</h2>
+        <p>{overview}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Movie;
